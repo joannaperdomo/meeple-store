44f5942cd4ec13f62889fcbab3ada61a4eeb7a6e
Joanna <joannaperdomo27@gmail.com>
2022-07-23 22:22:10 -0500

    completar el navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,14 +1,17 @@
-import logo from './die.png';
+import logo from './dado-logo.svg';
 
 function NavBar() {
     return (
         <div className='nav-bar'>
-            <img  className='App-logo' src={logo} alt="" />
+            <div className="Logo-container">
+                <img  className='App-logo' src={logo} alt="" />
+                <h1>Meeple Store</h1>
+            </div>
             <ul>
-                <li>Nosotros</li>
-                <li>Tienda</li>
-                <li>Mi Perfil</li>
-                <li>Cuenta</li>
+                <li><a href="#">Nosotros</a></li>
+                <li><a href="#">Tienda</a></li>
+                <li><a href="#">Ofertas</a></li>
+                <li><a href="#">Mi perfil</a></li>
             </ul>  
         </div>
     );
